3e4edb09c830582b36780bff40433ebab09c10f8
blodstone <hardy.oei@gmail.com>
2018-10-02 16:13:24 +0100

    Rebuild the project again

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask('__name__', static_folder='../dist', template_folder='dist')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
