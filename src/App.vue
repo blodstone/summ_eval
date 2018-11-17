512ca2a728de6b2fb5ed0bb6d5d3a5231a826b4c
blodstone <hardy.oei@gmail.com>
2018-11-17 05:58:43 +0000

    Add Bulma into vue and remove bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,60 @@
 <template>
   <div class="clearfix">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand" href="#">
+          <a class="navbar-item">Summ-Eval</a>
+        </div>
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item">
+                Home
+              </a>
+
+              <a class="navbar-item">
+                Documentation
+              </a>
+
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  More
+                </a>
+
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    About
+                  </a>
+                  <a class="navbar-item">
+                    Jobs
+                  </a>
+                  <a class="navbar-item">
+                    Contact
+                  </a>
+                  <hr class="navbar-divider">
+                  <a class="navbar-item">
+                    Report an issue
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <a class="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light">
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+      </nav>
     <router-view/>
   </div>
 </template>
-
 <style lang="scss">
-@import '../node_modules/bootstrap/scss/bootstrap.scss';
-body {
-  padding-top: 2rem;
-}
 @mixin clearfix() {
   &::after {
     display: block;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,5 @@
 <template>
   <div class="home">
-    <header>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Summ-Eval</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarCollapse" aria-controls="navbarCollapse"
-                aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <router-link to="/manage" class="nav-link">Manage Projects</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/status" class="nav-link">Status</router-link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
     <main class="container">
       <router-view></router-view>
     </main>
