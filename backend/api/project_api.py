1885979888bb71253add94cf7fcdecc5a9aabbea
blodstone <hardy.oei@gmail.com>
2018-11-20 17:37:31 +0000

    Split api.py to several files

--- backend/api/project_api.py
+++ backend/api/project_api.py
@@ -1,35 +1,11 @@
-import os
 import json
 import http
-import jwt
 import urllib.parse
-from datetime import datetime, timedelta
-from flask import Blueprint, jsonify, request
-from backend.models import User, Document, Project, AnnotationResult, Dataset, DocStatus, db
 
-api = Blueprint('api', 'api', url_prefix='', static_folder='../../instance/dist/static')
+from flask import jsonify, request
 
-
-# API
-@api.route('/document/<doc_id>', methods=['GET'])
-def api_document_get(doc_id):
-    if request.method == 'GET':
-        doc_json = json.dumps(Document.get_dict(doc_id))
-        if doc_json:
-            return jsonify(doc_json), http.HTTPStatus.Ok
-
-
-@api.route('/document/get_one', methods=['GET'])
-def api_document_get_one():
-    documents = Document.query.all()
-    for document in documents:
-        for doc_status in document.doc_statuses:
-            n_results = len(AnnotationResult.query.filter_by(id=doc_status.id).all())
-            if doc_status.total_exp_results == n_results:
-                continue
-            else:
-                return jsonify(document.to_dict())
-    return '', http.HTTPStatus.NO_CONTENT
+from . import api
+from backend.models import Document, Project, AnnotationResult, Dataset, DocStatus, db
 
 
 @api.route('/project/<project_id>/single_doc', methods=['GET'])
@@ -127,29 +103,6 @@ def api_project_progress():
             result_json['projects'].append(project_json)
         return jsonify(result_json)
 
-
-@api.route('/dataset/<dataset_name>', methods=['GET'])
-def api_dataset_get_single(dataset_name):
-    dataset = Dataset.query.filter_by(name=dataset_name).first()
-    if not dataset:
-        return '', http.HTTPStatus.NO_CONTENT
-    else:
-        return jsonify(dataset.to_dict())
-
-
-@api.route('/dataset', methods=['GET'])
-def api_dataset_get_names():
-    datasets = Dataset.query.all()
-    if not datasets:
-        return '', http.HTTPStatus.NO_CONTENT
-    else:
-        result = dict()
-        result['names'] = []
-        for dataset in datasets:
-            result['names'].append(dataset.name)
-        return jsonify(result)
-
-
 @api.route('/doc_status/progress/<doc_status_id>', methods=['GET'])
 def api_doc_status_progress(doc_status_id):
     doc_status = DocStatus.query.filter_by(id=doc_status_id).first()
@@ -158,30 +111,3 @@ def api_doc_status_progress(doc_status_id):
     n_results = len(AnnotationResult.query.filter_by(id=doc_status.id).all())
     progress = "{0:.2f}".format(n_results/doc_status.total_exp_results)
     return jsonify(dict(progress=progress))
-
-
-@api.route('/register/', methods=['POST'])
-def register():
-    data = request.get_json()
-    user = User(**data)
-    db.session.add(user)
-    db.session.commit()
-    return jsonify(user.to_dict()), 201
-
-
-@api.route('/login', methods=['POST'])
-def login():
-    data = request.get_json()
-    print(data)
-    user = User.authenticate(**data)
-    print(user)
-    if not user:
-        return jsonify({'message': 'Invalid credentials', 'authenticated': False}), 401
-    print('login success!')
-    token = jwt.encode({
-        'sub': user.email,
-        'iat': datetime.utcnow(),
-        'exp': datetime.utcnow() + timedelta(minutes=30)
-    }, api.config['SECRET_KEY']
-    )
-    return jsonify({'token': token.decode('UTF-8')})
